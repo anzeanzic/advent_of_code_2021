9c0c88f4630bbc8d1b2abb4abf3b1828406eae17
lostje <anze.anzic@siol.net>
2021-12-02 20:44:46 +0100

    Advent of Code 2021 Day 2

---
+++ 02_dive/02_dive_part_2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	ReadFile()
+}
+
+func ReadFile() {
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	horizontal_pos := 0
+	depth := 0
+	aim := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line_arr := strings.Fields(line)
+		direction := line_arr[0]
+		units, err := strconv.Atoi(line_arr[1])
+
+		if err != nil {
+			panic(err)
+		}
+
+		switch {
+		case direction == "forward":
+			horizontal_pos += units
+
+			if aim > 0 {
+				depth += units * aim
+			}
+		case direction == "up":
+			aim -= units
+		case direction == "down":
+			aim += units
+		}
+	}
+
+	fmt.Println("Horizontal position:", horizontal_pos, "Depth:", depth)
+	fmt.Println("Multiplied:", horizontal_pos*depth)
+
+	file.Close()
+}
